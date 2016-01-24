90259250f87ebeca4e5b1b99bde495adb5228d48
空樱酱 <ritsuka.sunny@gmail.com>
2016-01-24 12:13:07 +0800

    增加一个很蠢的方法用于解决异步问题

--- init.js
+++ init.js
@@ -1,85 +1,93 @@
-!(function() {
-    typechoYmplayer();
-})();
+var typechoYmplayer = {
+    idAllCount: [],
+    init: function() {
+        var that = this;
+        var ymplayerAll = document.getElementsByTagName("ymplayer");
+        var ymplayerCount = ymplayerAll.length;
+        if (ymplayerCount.length == 0) return;
 
-function typechoYmplayer() {
-    var ymplayerAll = document.getElementsByTagName("ymplayer");
-    if (ymplayerAll.length == 0) return;
+        for (var i = 0; i < ymplayerCount; i++) {
+            var ymplayer = ymplayerAll[i];
+            var field = ymplayer.attributes.field.value;
+            var idAll = that.toJSON(field);
+            that.idAllCount[i] = idAll.length;
+            if (typeof idAll['list'] == 'undefined' || typeof idAll['list'] == 'null') {
+                for (var u = 0, v = idAll.length; u < v; u++) {
+                    that.request({
+                        url: ymplayer_params.url + '?type=song&id=' + idAll[u],
+                        success: function(data) {
+                            var song = that.createSong(data);
 
-    for (var i = 0, l = ymplayerAll.length; i < l; i++) {
-        var ymplayer = ymplayerAll[i];
-        var field = ymplayer.attributes.field.value;
-        var idAll = toJSON(field);
-        if(typeof idAll['list'] == 'undefined' || typeof idAll['list'] == 'null'){
-            for (var u = 0, v = idAll.length; u < v; u++) {
-                request({
-                    url: ymplayer_params.url + '?type=song&id=' + idAll[u],
-                    success: function(data) {
-                        var song = document.createElement('song');
-                        song.attributes.setNamedItem(document.createAttribute('src'));
-                        song.attributes.setNamedItem(document.createAttribute('song'));
-                        song.attributes.setNamedItem(document.createAttribute('artist'));
-                        song.attributes.setNamedItem(document.createAttribute('cover'));
-                        song.attributes.setNamedItem(document.createAttribute('songid'));
-                        song.attributes.src.value = data.src;
-                        song.attributes.song.value = data.title;
-                        song.attributes.artist.value = data.artist;
-                        song.attributes.cover.value = data.cover;
-                        song.attributes.songid.value = data.song_id;
-
-                        request({
-                            url: ymplayer_params.url + '?type=lyric&id=' + song.attributes.songid.value,
-                            success: function(data) {
-                                if (data.lyric != 'not found') {
-                                    song.innerHTML = data.lyric;
-                                    ymplayer.appendChild(song);
+                            that.request({
+                                url: ymplayer_params.url + '?type=lyric&id=' + song.attributes.songid.value,
+                                success: function(data) {
+                                    if (data.lyric != 'not found') {
+                                        song.innerHTML = data.lyric;
+                                        ymplayer.appendChild(song);
+                                    }
                                 }
-                                console.log(u);
-                            }
-                        });
+                            });
 
+                        },
+                        error: function() {
+                            that.remove(ymplayer);
+                        }
+
+                    });
+                }
+            } else {
+                console.log("233");
+                request({
+                    url: ymplayer_params.url + '?type=playlist&playlist=' + idAll['list'],
+                    success: function(data) {
+                        ymplayer.innerHTML = data;
                     },
                     error: function() {
-                        remove(ymplayer);
+                        that.remove(ymplayer);
                     }
-
                 });
             }
+
         }
-        else
-        {
-            console.log("233");
-            request({
-                url: ymplayer_params.url + '?type=playlist&playlist=' + idAll['list'],
-                async: false,
-                success: function(data) {
-                    ymplayer.innerHTML = data;
-                },
-                error : function(){
-                    remove(ymplayer);
+        var timer = setInterval(function() {
+            for (var i = 0; i < ymplayerCount; i++) {
+                if (ymplayer.getElementsByTagName('song').length == that.idAllCount[i]) {
+                    ymplayerAll[i].style.display = '';
+                } else {
+                    return;
                 }
-            });
-        }
+            }
+            YmplayerIniter();
+            clearInterval(timer);
+        }, 1000);
 
-    }
-    for (var i = 0, l = ymplayerAll.length; i < l; i++) {
-        ymplayerAll[i].style.display = '';
-    }
-    YmplayerIniter();
-
-    function remove(element) {
+    },
+    createSong: function(data) {
+        var song = document.createElement('song');
+        song.attributes.setNamedItem(document.createAttribute('src'));
+        song.attributes.setNamedItem(document.createAttribute('song'));
+        song.attributes.setNamedItem(document.createAttribute('artist'));
+        song.attributes.setNamedItem(document.createAttribute('cover'));
+        song.attributes.setNamedItem(document.createAttribute('songid'));
+        song.attributes.src.value = data.src;
+        song.attributes.song.value = data.title;
+        song.attributes.artist.value = data.artist;
+        song.attributes.cover.value = data.cover;
+        song.attributes.songid.value = data.song_id;
+        return song;
+    },
+    remove: function(element) {
         element.parentNode.removeChild(element);
-    }
-
-
-    function request(o) {
+    },
+    request: function(o) {
+        var that = this;
         if (!o.url)
             return;
         var xmlhttp = new XMLHttpRequest() || new ActiveXObject('Microsoft.XMLHTTP');
         xmlhttp.onreadystatechange = function() {
             if (xmlhttp.readyState == 4 && xmlhttp.status == 200 && !!o.success) {
                 if (xmlhttp.responseText.match("^\{(.+:.+,*){1,}\}$")) {
-                    o.success(toJSON(xmlhttp.responseText));
+                    o.success(that.toJSON(xmlhttp.responseText));
                 } else {
                     o.success(xmlhttp.responseText);
                 }
@@ -88,13 +96,16 @@ function typechoYmplayer() {
             if (xmlhttp.readyState == 4 && xmlhttp.status != 200 && !!o.error)
                 o.error();
         };
-        xmlhttp.open('GET', o.url, false);
+        xmlhttp.open('GET', o.url, true);
         xmlhttp.setRequestHeader('X-Requested-With', 'XMLHttpRequest');
         xmlhttp.setRequestHeader('If-Modified-Since', '0');
         xmlhttp.send(null);
-    }
-
-    function toJSON(str) {
+    },
+    toJSON: function(str) {
         return eval("(" + str + ")");
     }
-}
+};
+
+!(function() {
+    typechoYmplayer.init();
+})();
