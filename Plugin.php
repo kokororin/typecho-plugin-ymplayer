d9d847260266880b09125698af94bcc968eac7a4
kirainmoe <i@imim.pw>
2017-02-01 22:43:09 +0800

    Fix the problem of submodule could't be downloaded with plugin zip

--- Plugin.php
+++ Plugin.php
@@ -19,6 +19,8 @@ class ymplayer_Plugin implements Typecho_Plugin_Interface
 {
 
     const PLUGIN_VERSION = '0.7';
+    const PLUGIN_SUPPORT_LINK = 'https://github.com/kokororin/typecho-plugin-ymplayer/blob/master/README.md#安装方法';
+    const PLUGIN_SUBMODULE_DOWNLOAD = 'https://raw.githubusercontent.com/kirainmoe/ymplayer/master/dist/assets/ymplayer.js';
 
     public static function activate()
     {
@@ -41,8 +43,40 @@ class ymplayer_Plugin implements Typecho_Plugin_Interface
     public static function config(Typecho_Widget_Helper_Form $form)
     {
         $element = new Typecho_Widget_Helper_Form_Element_Textarea(
-            'custom', null, '', '自定义样式', '请直接输入css代码，请不要带&lt;style&gt;标签');
+            'custom', null, '', '自定义样式', '请直接输入 CSS 代码，不要带&lt;style&gt;标签');
         $form->addInput($element);
+
+        if (!file_exists(dirname(__FILE__) . '/submodules/ymplayer/dist/assets/ymplayer.js')) {
+            echo '<h3>检测到 YMPlayer submodule 未被正确安装 _(:з」∠)_。';
+            if (is_writeable(dirname(__FILE__))) {
+                try {
+                    /* Create asset file directory */
+                    mkdir(dirname(__FILE__) . '/submodules/ymplayer/dist', 755);
+                    mkdir(dirname(__FILE__) . '/submodules/ymplayer/dist/assets', 755);
+
+                    /* Fetch latest source from GitHub */
+                    $ch = curl_init();
+                    curl_setopt($ch, CURLOPT_URL, self::PLUGIN_SUBMODULE_DOWNLOAD);
+                    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+                    $res = curl_exec($ch);
+
+                    /* Write source file */
+                    $src = fopen(dirname(__FILE__) . '/submodules/ymplayer/dist/assets/ymplayer.js', 'w+');
+                    fwrite($src, $res);
+                    curl_close($ch);
+
+                    echo '插件已尝试自动安装。请刷新本页面，如果这条提示消失，则说明 submodule 安装成功。';
+                    echo '否则，请前往 <a href="' . self::PLUGIN_SUPPORT_LINK . '">GitHub</a> 阅读教程手动安装。</h3>';
+                } catch (Exception $e) {
+                    echo '插件已尝试自动安装，但由于未知原因并没有成功。';
+                    echo '请前往 <a href="' . self::PLUGIN_SUPPORT_LINK . '">GitHub</a> 阅读教程手动安装。</h3>';
+                }
+            } else {
+                /* have no writing permission */
+                echo '当前目录不支持写入文件，插件无法自动安装子模块。';
+                echo '请前往 <a href="' . self::PLUGIN_SUPPORT_LINK . '">GitHub</a> 阅读教程手动安装。</h3>';
+            } // is_writeable()
+        } // file_exist()
     }
 
     public static function personalConfig(Typecho_Widget_Helper_Form $form)
@@ -107,7 +141,7 @@ class ymplayer_Plugin implements Typecho_Plugin_Interface
     {
         $package = file_get_contents(__DIR__ . '/submodules/ymplayer/package.json');
         $package = json_decode($package);
-        return $package->version;
+        return (!empty($package->version) ? $package->version : '4.0');
     }
 
 }
